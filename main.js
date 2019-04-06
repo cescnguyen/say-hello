de0261202d9e4ec276032d1eafc5b2a9d111d40d
cescnguyen <ducvnmail@gmail.com>
2019-04-07 00:15:48 +0700

    new function file main

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ function Test(name) {
     this.name = name;
 }
 
-function Test1() {
+function Test1(name) {
+    this.name = name
 }
 
-var tt = new Test();
+var A = new Test('Nguyen Van A');
+
+var B = new Text1('Nguyen Van B');
+
+var C = new Text1('Nguyen Van c');
