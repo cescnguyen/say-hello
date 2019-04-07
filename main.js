eccfb35c218153cf4c4eafafee708427d3a01cc0
cescnguyen <ducvnmail@gmail.com>
2019-04-08 00:28:39 +0700

    add khoi khoi

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-function Test(name) {
-    this.name = name;
-}
-
-function Test1(name) {
-    this.name = name
-}
-
-var A = new Test('Nguyen Van A');
-
-var B = new Text1('Nguyen Van B');
-
-var C = new Text1('Nguyen Van c');
+// test
