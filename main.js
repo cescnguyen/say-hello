cf7aaf3aaac260f4c9ec2476f95cedd7d5bbae05
cescnguyen <ducvnmail@gmail.com>
2019-04-08 00:23:43 +0700

    clear conflit

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-function Test() {
+function Test(name) {
+    this.name = name;
 }
+
+function Test1(name) {
+    this.name = name
+}
+
+var A = new Test('Nguyen Van A');
+
+var B = new Text1('Nguyen Van B');
+
+var C = new Text1('Nguyen Van c');
