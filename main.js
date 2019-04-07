b2d88f9729bd321d68fc0d4f4599fa539c0b67c6
cescnguyen <ducvnmail@gmail.com>
2019-04-08 00:15:15 +0700

    readd file mainjs

--- main.js
+++ main.js
@@ -2,12 +2,7 @@ function Test(name) {
     this.name = name;
 }
 
-function Test1(name) {
-    this.name = name
+function Test1() {
 }
 
-var A = new Test('Nguyen Van A');
-
-var B = new Text1('Nguyen Van B');
-
-var C = new Text1('Nguyen Van c');
+var tt = new Test();
